82947b4be47866d38a82a749a5df573b592f99d8
rival7008 <antonasberg9@gmail.com>
2022-10-16 15:45:50 +0200

    Initial commit

---
+++ Internet_Bank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Internet_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123");
+        }
+    }
+}
