6806966cd0672bc18be0e06205e4dd87c77732b9
rival7008 <antonasberg9@gmail.com>
2022-10-16 15:52:14 +0200

    la till konton i en array

--- Internet_Bank/Program.cs
+++ Internet_Bank/Program.cs
@@ -6,7 +6,18 @@ namespace Internet_Bank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("123");
+            
+        }
+
+        public static string[,] users()
+        {
+            String[,] Users = new string[5, 2];
+            Users[0, 0] = "user1"; Users[0, 1] = "1111";
+            Users[1, 0] = "user2"; Users[1, 1] = "2222";
+            Users[2, 0] = "user3"; Users[2, 1] = "3333";
+            Users[3, 0] = "user4"; Users[3, 1] = "4444";
+            Users[4, 0] = "user5"; Users[4, 1] = "5555";
+            return Users;
         }
     }
 }
